ef3cb64828751d84f75815a9874d39e9901597d0
saurav-commits <sauravsky55@gmail.com>
2023-07-24 10:15:19 +0530

    Auth Practice

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const router = require('./routes/auth-routes');
+
+const app = express();
+
+// set view engine
+app.set('view engine', 'ejs');
+app.use('/auth',authRoutes);
+
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('app now listening for requests on port 3000');
+});
---
+++ routes/auth-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+
+// auth login
+router.get('/login', (req,res)=>{
+    res.render('login');
+});
+
+// auth with google
+router.get('/google', (req, res)=> {
+    // handle with passport
+    res.send('logging in with google');
+})
+
+
+// auth logout
+router.get('/logout', (req, res)=>{
+    res.send('logging out');
+})
+
+module.exports = router;
